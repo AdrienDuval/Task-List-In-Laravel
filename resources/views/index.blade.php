20efc9a20b32b3d342baf13b5ca906b5b9fc1610
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-07-13 02:56:08 +0530

    Working on the layout template and other codes in laravel
    -how to create a common layout in laravel(resource-layout-app.blade.php)
    
    -used the collect method to convert arrays to objects(in php arrays are not objects like in php), so we convert it to be able to display the objects value easily
    
    -used the redirect route for the default template
    
    -used the @extends, @sections, @yeilds
    --@extend is used to load the layout on the blade template page
    --@yeild is used in the layout template to specify where the code from the view template will be added in the layout template
    --@section is used for to specify which parts will be redered in the yeild in the layout template it take the same argument passed in the yeild
    
    that is if the @yeild('content') having a parameter of content the the section have the content that will be defined in the section as @section('contetn')

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,19 @@
-<h1>Blade template</h1>
+@extends('Layouts.app');
 
-{{-- @isset($task)
-    @foreach ($task as $tasks)
-    {{ print_r($task) }}
-    @endforeach
-@endisset --}}
+@section('title', ' Blade template')
 
-<div class="container">
+@section('content')
+    <div class="container">
 
-    @forelse ($task as $tasks)
-        <div style="margin-bottom:30px;">
-            <a href="{{ route('tasks.show', ['id' => $tasks->id]) }}"> {{ $tasks->title }} <br> {{ $tasks->description }} <br> {{ $tasks->long_description }}</a>
-        </div>
-    @empty
-        <div>
-            <h4>the list is empty</h4>
-        </div>
-    @endforelse
+        @forelse ($tasks as $task)
+            <div style="margin-bottom:30px;">
+                <a href="{{ route('tasks.show', ['id' => $task->id]) }}"> {{ $task->title }}</a>
+            </div>
+        @empty
+            <div>
+                <h4>the list is empty</h4>
+            </div>
+        @endforelse
 
-    {{-- @if (count($task))
-        @foreach ($task as $tasks)
-           <div> {{ $tasks->id }} </div>
-        @endforeach
-    @else
-        <div>there are no tasks!</div>
-    @endif --}}
-</div>
+    </div>
+@endsection
