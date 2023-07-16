d46b4f9f64c96bb3c47ba8668266637717516502
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-07-16 14:21:47 +0530

    sessions, flash messgaes
    how to store validation errors in a sessions
    
    -it is recommended to save your sessions in laravel in either redis or memcached, as the allow us to migrate the sessions form one server to another and provide very quick access. storing session is not good if you app is big as they will not allow you to migrate the sessions to different serves
    
    -the @error() Blade directive in Laravel is used to display error messages for a specific form field. The directive takes the name of the form field as its argument. For example, the following code would display an error message for the title form field if there is an error for that field:
    
    ==========
    @error('title')
        <p class="error-message">{{ $message }}</p>
    @enderror
    ==========
    
    -The @error() directive will only render the error message if there is an error for the specified form field. If there are no errors, the directive will not render anything.
    
    The {{ $message }} Blade directive is used to display the error message. The error message is typically a string that is generated by the Laravel validation system.
    
    -we can also load internal style using the @section in the blade template and the @yeild in the layout
    
    - Displaying flash messages, A flash message is a temporary message that is displayed to the user after a successful or unsuccessful operation.
    The with() Blade directive is used to set a flash message. The directive takes two arguments: the name of the flash message and the value of the flash message.
    
    fron the code bellow
    1-The with() Blade directive sets the flash message with the name success and the value Task created succesfully!.
    
    2-The @if Blade directive checks if the session has a flash message with the name success. If the session does have a flash message with the name success, the @if directive will render the <div class="alert alert-success"> HTML tag.
    
    3-The {{ session('success') }} Blade directive will get the flash message from the session and render it to the user.
    
    4-The @endif Blade directive marks the end of the @if directive.
    
    @if (session()->has('success'))
                <div class="alert alert-success">
                    {{ session('success') }}
                </div>
            @endif

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 10 Task List App</title>
 </head>
+
 <body>
-    <h1>@yield('title')</h1>
+    <h1><u>@yield('title')</u></h1>
+    @yield('styles')
     <div>
+        @if (session()->has('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -47,5 +47,5 @@ Route::post('/tasks', function (request $request) {
   $task->long_description = $data['long_description'];
   $task->save();
 
-  return redirect()-> route('tasks.show', ['id' => $task->id]);
+  return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task created succesfully!');
 })->name('tasks.store');
