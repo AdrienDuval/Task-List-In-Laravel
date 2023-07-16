15111576b7761d27b3f3099d8dc2c01189b7f641
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-07-16 13:57:23 +0530

    validation
    validation
    
    {{ $errors }} ::
    The {{ $errors }} Blade directive in Laravel is used to display error messages for form fields. The directive will render a list of all of the error messages for the current form.
    
    The {{ $errors }} directive is typically used in conjunction with the @if directive. For example, the following code would display error messages for the name and email form fields if there are any errors:

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use \App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,19 +21,31 @@ Route::get('/', function () {
   return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function ()  {
-    return view('index', [
-        'tasks' => \App\Models\Task::latest()->get()
-    ]);
+Route::get('/tasks', function () {
+  return view('index', [
+    'tasks' => Task::latest()->get()
+  ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')
-->name('tasks.create');
+  ->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id) {
-  return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
+  return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
-Route::post('/tasks', function ( request $request ) {
-  dd($request->all());
+Route::post('/tasks', function (request $request) {
+  $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+  ]);
+
+  $task = new Task();
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+  $task->save();
+
+  return redirect()-> route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
