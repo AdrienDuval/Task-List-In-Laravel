635040ceb0eb4b359745bb88336e7e9618f9513d
adrien-raciocinative <annickalan67@gmail.com>
2023-07-26 20:44:34 +0530

    Adding paginations
    paginations
    
    The  paginate()  method in Laravel is used for paginating query results. It allows you to split a large set of data into smaller, more manageable chunks called "pages". This is particularly useful when you have a large dataset and want to display it in a user-friendly manner, with pagination links to navigate between pages.
    
    Here's a detailed explanation of the  paginate()  method:
    
    1. Retrieving a Query Builder or Eloquent Model:
       The  paginate()  method can be used on a query builder instance or an Eloquent model. For example, you can use it on a model like  Task  as shown below:
    $tasks = Task::paginate(10);
    2. Specifying the Number of Items per Page:
       The  paginate()  method takes an integer argument that represents the number of items to display per page. In the example above,  10  is passed as the argument, which means each page will display 10 tasks.
    
    3. Paginated Results:
       The  paginate()  method returns a  LengthAwarePaginator  instance, which contains the paginated results along with additional information about the pagination. This includes the total number of items, the current page, and other helpful methods and properties.
    
    4. Displaying Paginated Results in a View:
       After retrieving the paginated results, you can pass them to a view for rendering. For example:
    return view('index', ['tasks' => $tasks]);
    5. Displaying Pagination Links:
       In your view, you can use the  links()  method on the paginated results to generate the pagination links. Place the following code where you want the pagination links to appear:
       {{ $tasks->links() }}
    
    This will generate the necessary HTML markup for the pagination links, including previous, next, and page number links.
    
    By using the  paginate()  method, you can easily implement pagination in Laravel, allowing you to display large datasets in a user-friendly way and navigate between pages of data.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,18 @@
 
         @forelse ($tasks as $task)
             <div style="margin-bottom:30px;">
-                <a href="{{ route('tasks.show', ['task' => $task->id]) }}"> {{ $task->title }}, <b>ID: {{ $task->id }}</b></a>
+                <a href="{{ route('tasks.show', ['task' => $task->id]) }}"> {{ $task->title }}, <b>ID:
+                        {{ $task->id }}</b></a>
             </div>
         @empty
             <div>
                 <h4>the list is empty</h4>
             </div>
         @endforelse
+
+        @if ($tasks->count())
+            <nav>{{ $tasks->links() }}</nav>
+        @endif
 
     </div>
 @endsection
