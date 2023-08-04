39e1b630bdc82bcdf457a6ff9e383765a2f79156
adrien <annickalan67@gmail.com>
2023-08-04 10:03:34 +0530

    Styling and adding interaction with Alpiine.js library
    Learned how  to use Alpine.js and styling the flash messages in the full project

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -24,14 +24,33 @@
         </style>
     {{-- blade-formatter-enable --}}
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 
 <body class="container mx-auto mt-10 mb-10 max-w-2xl">
     <h1 class="text-2xl mb-4">@yield('title')</h1>
-    <div>
+    <div x-data="{ flash: true }">
         @if (session()->has('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
+            <div x-show="flash"
+                class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800 relative"
+                role="alert">
+                <svg @click="flash = false" class="flex-shrink-0 inline absolute top-0 right-2 pt-2 cursor-pointer"
+                    fill="#e53e3e" height="20" width="20" version="1.1" id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 490 490"
+                    xml:space="preserve">
+                    <polygon
+                        points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490 
+	489.292,457.678 277.331,245.004 489.292,32.337 " />
+                </svg>
+                <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span class="sr-only">Info</span>
+                <div>
+                    <span class="font-medium">Success alert!</span> {{ session('success') }}
+                </div>
             </div>
         @endif
         @yield('content')
